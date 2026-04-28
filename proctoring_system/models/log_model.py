fed6a6aa68ab92961f3448a1f478490ee4d5dd53
Dilan-Steve-Rebolledo-Vasquez <260524286+Dilan-Steve-Rebolledo-Vasquez@users.noreply.github.com>
2026-04-28 07:59:01 -0500

    Prototipo

--- proctoring_system/models/log_model.py
+++
@@ -1,9 +0,0 @@
-from models import db
-from datetime import datetime
-
-class ExamLog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False)
-    event_type = db.Column(db.String(50))
-    details = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
